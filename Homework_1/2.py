6dceab4441a6245f2096f2539250304e04c955ea
Deathwing1998 <49477208+Deathwing1998@users.noreply.github.com>
2020-05-27 13:43:29 +0300

    Create 2.py

---
+++ Homework_1/2.py
@@ -0,0 +1,10 @@
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+d = float(input('d: '))
+f = float(input('f: '))
+
+if f - d:
+    print((a * b - c) / (f - d))
+else:
+    print('Делить на ноль нельзя!')
