ae21097d9714d4ed574f3f53cd6ca6e64fd31e67
Deathwing1998 <49477208+Deathwing1998@users.noreply.github.com>
2020-05-27 14:48:23 +0300

    Update 3.py

--- Homework_1/3.py
+++ Homework_1/3.py
@@ -3,6 +3,5 @@ import math
 x = float(input('x: '))
 y = float(input('y: '))
 z = float(input('z: '))
-
-result = math.sqrt(x**2 + y**2 + z**2)
-print(result)
+а = x**2 + y**2 + z**2
+print(math.sqrt(a))
