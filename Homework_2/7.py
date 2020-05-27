8cadfb57103a884626835326fc086ba8a27a728f
Deathwing1998 <49477208+Deathwing1998@users.noreply.github.com>
2020-05-27 16:13:00 +0300

    Update 7.py

--- Homework_2/7.py
+++ Homework_2/7.py
@@ -1,13 +1,10 @@
 from random import randint
-
-random_computer_num = randint(0, 100)
-user_num = int(input('num: '))
-
-while user_num != random_computer_num:
-    if user_num > random_computer_num:
-        print('The guessed number is less!')
+rand = randint(0, 100)
+image = int(input('Load = '))
+while image != rand:
+    if image > rand:
+        print("Chislo less than yours")
     else:
-        print('The guessed number is bigger!')
-    user_num = int(input('num: '))
-print('You guessed the number!')
-print('The guessed number is ' + str(random_computer_num))
+        print("Chislo bigger than yours")
+    umage = int(input('Load = '))
+print("Congratulations!You found it")
