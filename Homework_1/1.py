4ef253df1647665f96579fb9400bade592c72547
Deathwing1998 <49477208+Deathwing1998@users.noreply.github.com>
2020-05-27 14:33:29 +0300

    Update 1.py

--- Homework_1/1.py
+++ Homework_1/1.py
@@ -2,8 +2,8 @@ x = float(input('x: '))
 y = float(input('y: '))
 
 if x > y:
-    print('1-ое число больше 2-ого')
+    print('x больше y')
 elif y > x:
-    print('2-ое число больше 1-ого')
+    print('y больше x')
 else:
     print('Числа равны')
